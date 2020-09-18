baf84e82726b325d8119aa0ccaaa7cbe9989da00
Ikuo Matsumura <ikuo@initials.im>
2020-09-18 23:16:18 +0900

    Reduce noise on homing

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2469,11 +2469,11 @@
    */
   #define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     100  // [mm/s]
+  #define X_HYBRID_THRESHOLD     400  // [mm/s]
   #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     100
+  #define Y_HYBRID_THRESHOLD     400
   #define Y2_HYBRID_THRESHOLD    100
-  #define Z_HYBRID_THRESHOLD       3
+  #define Z_HYBRID_THRESHOLD      10
   #define Z2_HYBRID_THRESHOLD      3
   #define Z3_HYBRID_THRESHOLD      3
   #define Z4_HYBRID_THRESHOLD      3
